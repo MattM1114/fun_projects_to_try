9f2a328911c0c33a0c11cf6d65e545ff6ce7bd9c
Matthew Tadiwa Magadza <64587393+MattM1114@users.noreply.github.com>
2023-02-14 23:10:05 +0200

    Update day3.py

--- day3.py
+++ day3.py
@@ -166,13 +166,55 @@ else:
     print(f"your love score is {love_score}")
 '''
 
-# choose your own adventure game 
+# choose your own adventure game
+
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+# https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+# Write your code below this line 👇
+choice1 = input(
+    "wich way do you wnat to go if left press l if right press r?\n")
 
 
-print(" Welcome to pick your own adventure game ")
-
-right = input("if you want to go right press r ")
-left = input("if you want to go left press l ")
-swim = input("if you want to swim press s ")
-wait = input("if you want to wait for a ship press w")
-red = input("")
+if choice1 == "r":
+    print("you were attacked by a troll and died.")
+    quit
+elif choice1 == "l":
+    print("Lou come to a lake there is an old bout doked by at the river.")
+    choice2 = input(
+        "how will you cross the river if you swim press s , if you take the bout press b?\n")
+    if choice2 == "b":
+        print("You made it across safely and you docked at a castle with three doors.")
+        choice3 = input(
+            "wich door will you pick if it is the red door press r if it is the yellow door press y if it is the blue door press b?\n")
+        if choice3 == "b" or choice3 == "y":
+            print("you were killed by an evil wizerd's curse that ate your flesh.")
+            quit
+        elif choice3 == "r":
+            print("you found the treasure and you are now the hier to the kingdom.")
+    elif choice2 == "s":
+        print("A mermaid has draged you under the water and you drowned and eaten")
