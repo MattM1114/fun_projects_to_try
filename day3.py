695cbcf570488d88fe8edb4d8c449f9ccc2ddd45
Matthew Tadiwa Magadza <64587393+MattM1114@users.noreply.github.com>
2023-02-20 23:33:13 +0200

    update for day3 and day5

--- day3.py
+++ day3.py
@@ -193,28 +193,22 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
 
-# https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
-
-# Write your code below this line 👇
 choice1 = input(
-    "wich way do you wnat to go if left press l if right press r?\n")
-
-
-if choice1 == "r":
-    print("you were attacked by a troll and died.")
-    quit
-elif choice1 == "l":
-    print("Lou come to a lake there is an old bout doked by at the river.")
+    'You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
+if choice1 == "left":
     choice2 = input(
-        "how will you cross the river if you swim press s , if you take the bout press b?\n")
-    if choice2 == "b":
-        print("You made it across safely and you docked at a castle with three doors.")
-        choice3 = input(
-            "wich door will you pick if it is the red door press r if it is the yellow door press y if it is the blue door press b?\n")
-        if choice3 == "b" or choice3 == "y":
-            print("you were killed by an evil wizerd's curse that ate your flesh.")
-            quit
-        elif choice3 == "r":
-            print("you found the treasure and you are now the hier to the kingdom.")
-    elif choice2 == "s":
-        print("A mermaid has draged you under the water and you drowned and eaten")
+        'You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
+    if choice2 == "wait":
+        choice3 = input("You arrive at the island unharmed. There is a house")
+        if choice3 == "red":
+            print("It's a room full of fire. Game Over.")
+        elif choice3 == "yellow":
+            print("You found the treasure! You Win!")
+        elif choice3 == "blue":
+            print("You enter a room of beasts. Game Over.")
+        else:
+            print("You get attacked by an angry trout. Game Over.")
+    else:
+        print("You get attacked by an angry trout. Game Over.")
+else:
+    print("You fell into a hole. Game Over.")
--- day5.py
+++ day5.py
@@ -1,3 +1,4 @@
+import random
 # eg 0 loops
 # For Loop with Lists
 """
@@ -10,8 +11,8 @@ print(fruits)
 
 # ex 0 average height of students
 # This will calculate the average height of a number of people using for loops and lists.
-
-
+# I was not allowed to use the len() and sum() functions because it would not help me learn about for loops and would be to easy.
+'''
 student_heights = input("Input a list of student heights ").split()
 for n in range(0, len(student_heights)):
     student_heights[n] = int(student_heights[n])
@@ -28,3 +29,105 @@ for students in student_heights:
 
 average = round(total_height/number_of_students)
 print(average)
+'''
+
+# ex 1 best score
+# this program use a forloop to figure out the best score for a number of student like the max() function would
+'''
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+    student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+# Write your code below this row 👇
+
+max_score = 0
+for score in student_scores:
+    if score > max_score:
+        max_score = score
+
+
+print(f"The highest score in the class is: {max_score}")
+'''
+# eg 1 range()
+# this program figures out the total of all the numbers from 1 - 100 which is 5050
+'''
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
+'''
+# ex 2 this program figures out the total of even numbers from 1 - 100 which is 2550
+''' 
+total = 0
+for number in range(0, 101, 2):
+    total += number
+print(total)
+'''
+# if you want total of the odd numbers from 1 - 100 which is 2500 then you would do this:
+'''
+total = 0
+for number in range(1, 101, 2):
+    total += number
+print(total)
+'''
+# ex 3 fizzbuzz
+'''
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
+'''
+
+# ex 4 Password Generator Project
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+           'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+# Easy Level - Order not randomized:
+# e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+'''
+password = ""
+
+for char in range(1, nr_letters + 1): 
+    password += random.choice(letters)
+
+for sym in range(1, nr_symbols + 1):
+    password += random.choice(symbols)
+
+for num in range(1, nr_numbers + 1): 
+    password += random.choice(numbers)
+
+
+print(password)
+'''
+# Hard Level - Order of characters randomized:
+# e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+password_list = []
+
+for char in range(1, nr_letters + 1):
+    password_list.append(random.choice(letters))
+
+for sym in range(1, nr_symbols + 1):
+    password_list.append(random.choice(symbols))
+
+for num in range(1, nr_numbers + 1):
+    password_list.append(random.choice(numbers))
+
+print(password_list)
+random.shuffle(password_list)
+password = "".join(password_list)
+print(password)
