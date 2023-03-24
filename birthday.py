11a35f857d64708412276ed88346aa60cf4c4324
Matthew Tadiwa Magadza <64587393+MattM1114@users.noreply.github.com>
2023-03-24 11:14:09 +0200

    day 16

--- birthday.py
+++ birthday.py
@@ -6,31 +6,48 @@ name = input("Enter a name: ")
 # set up turtle
 t = turtle.Turtle()
 t.speed(0)
+t.hideturtle()
+
+# define function for drawing a flame
+def draw_flame(size):
+    t.color("yellow", "red")
+    t.begin_fill()
+    t.right(45)
+    t.forward(size)
+    t.right(90)
+    t.forward(size)
+    t.right(90)
+    t.forward(size)
+    t.right(90)
+    t.forward(size)
+    t.right(135)
+    t.forward(size * 0.8)
+    t.end_fill()
 
 # draw cake base
 t.penup()
-t.goto(0, -100)
+t.goto(0, -150)
 t.pendown()
 t.begin_fill()
-t.color("orange")
-t.circle(100)
+t.color("#FFBF00")
+t.circle(150)
 t.end_fill()
 
 # draw cake icing
 t.penup()
-t.goto(0, -50)
+t.goto(0, -120)
 t.pendown()
 t.begin_fill()
-t.color("pink")
-t.circle(50)
+t.color("#FFE5B4")
+t.circle(120)
 t.end_fill()
 
-# draw candles
+# draw candles and flames
+t.color("#FFD700")
 for i in range(8):
     t.penup()
-    t.goto(-50 + i * 14, 20)
+    t.goto(-50 + i * 20, 70)
     t.pendown()
-    t.color("bl")
     t.begin_fill()
     t.forward(10)
     t.left(90)
@@ -38,11 +55,25 @@ for i in range(8):
     t.left(90)
     t.forward(10)
     t.end_fill()
+    t.penup()
+    t.goto(-45 + i * 20, 100)
+    t.pendown()
+    draw_flame(8)
 
-# write message
+# draw message
 t.penup()
-t.goto(0, 100)
-t.color("black")
-t.write("Happy Birthday, {}!".format(name), align="center", font=("Arial", 24, "bold"))
+t.goto(0, 220)
+t.color("#A91E1E")
+t.write("Happy Birthday, {}!".format(name), align="center", font=("Arial", 36, "bold"))
+
+# add custom message
+t.penup()
+t.goto(0, -50)
+t.color("#A91E1E")
+t.write("You're amazing!", align="center", font=("Arial", 24, "bold"))
+
+# print birthday wish and compliment
+print("Happy birthday, {}! I hope your day is filled with joy and happiness. You're an amazing person and you deserve the best!".format(name))
 
 turtle.done()
+
--- day16/day16.py
+++ day16/day16.py
@@ -1,5 +1,33 @@
 #import turtle 
+"""
+import turtle 
+# Turtle is a class
+timmy = turtle.Turtle()
+print(timmy)
+#shape and color are methods 
+timmy.shape("turtle")
+timmy.color("green")
+timmy.forward(100)
 
-from turtle import Turtle
-# Turtle is a cl
-timmy = Turtle()
+my_screen = turtle.Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+"""
+# https://pypi.org/ this were people
+# put all their python 
+# to install a package I would search it on the website 
+# https://pypi.org/ then I would type pip install <package name>
+# in the terminal when it is open in powershell
+"""
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name",["pikachu","squrtle", "charmander" ], )
+table.add_column("type",["electric", "water", "fire"])
+
+table.align = "l"
+
+print(table)
+"""
+# https://www.rollingstone.com/culture/culture-news/steve-jobs-in-1994-the-rolling-stone-interview-231132/
+# 
+
