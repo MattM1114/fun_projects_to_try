75e20a887318419eafadd0b14df5c8342a8353b9
Matthew Tadiwa Magadza <64587393+MattM1114@users.noreply.github.com>
2024-02-29 12:51:12 +0200

    fix

--- day20 & day21/score.py
+++ day20 & day21/score.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+import os
 ALIGNMENT = "center"
 FONT = ("Arial", 24, "normal")
 
@@ -7,8 +8,19 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        with open("data.txt")as data:
-            self.high_score =  int(data.read())
+        
+        script_directory = os.path.dirname(os.path.realpath(__file__))
+        data_file_path = os.path.join(script_directory, "data.txt")
+        try:
+            with open(data_file_path)as data:
+                self.high_score =  int(data.read())
+        except FileNotFoundError:
+            print("High score file not found. Starting with high score of 0.")
+        except ValueError:
+            print("Error reading high score. Defaulting to 0.")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+        
         self.color("white")
         self.penup()
         self.goto(0, 270)
