dad8231f6754e970227f5f2ae770792cb9750cc0
Matthew Tadiwa Magadza <64587393+MattM1114@users.noreply.github.com>
2025-03-04 10:59:02 +0200

    topic 1 c++

---
+++ topic1.cpp
@@ -0,0 +1,17 @@
+// this is demo of c++ program stucture
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int num1,num2;
+	num1 = 10;
+	num2 = 20;
+	int num3 = num1 + num2;
+	string name;
+	cin >> name;
+
+	cout << "hello "<< name <<" the number is "<<num3;
+	
+	system("pause>0");
+}
